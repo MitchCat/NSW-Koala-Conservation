3ae5edd7d1758c8afc25b2efc5f7fd952c7057f8
Mitchell Bruce Catlin <mitchellbcatlin@gmail.com>
2025-10-26 06:50:24 +1100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+git clone https://github.com/YOUR-USERNAME/nsw-koala-conservation.git
+cd nsw-koala-conservation
+echo "// Google Earth Engine app for NSW Koala Conservation" > README.md
+# paste your script in app.js
+git add .
+git commit -m "Initial Earth Engine web app upload"
+git push origin main
